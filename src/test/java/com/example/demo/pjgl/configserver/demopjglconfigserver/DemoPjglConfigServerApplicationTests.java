a3361aa2fea2aa9f5dc7e06580d868b88f356609
Paúl Jheferson Guevara Linares <pguevarl@emeal.nttdata.com>
2024-02-09 16:16:29 -0500

    first commit

---
+++ src/test/java/com/example/demo/pjgl/configserver/demopjglconfigserver/DemoPjglConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo.pjgl.configserver.demopjglconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoPjglConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
